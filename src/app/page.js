ce1a757f1b9871162545c6bcc77f40ce32419c7d
longinus99 <longinus723@gmail.com>
2025-04-21 01:35:50 +0900

    테스트용

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>서울 날씨</h1>
-      <p className={styles.desc}>현재 맑음 🌤️</p>
+      <p className={styles.desc}>현재 맑니? 🌤️</p>
     </div>
   )
 }
